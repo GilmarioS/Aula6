eca7c61b773b679158a18fb0cd70a2e132379c07
Gilmario Santos <102427018+GilmarioS@users.noreply.github.com>
2023-11-04 14:12:41 -0300

    Update Cliente.java
    Atualização no codigo cliente adicionados os campos  email, telefone

--- src/main/java/com/aul6/api/model/Cliente.java
+++ src/main/java/com/aul6/api/model/Cliente.java
@@ -5,31 +5,63 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 @Entity
+@Schema(description = "Detalhes do cliente")
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID do cliente")
     private Long id;
 
     @Column(nullable = false)
+    @Schema(description = "Nome do cliente")
     private String nome;
 
-    public String getNome() {
-        return nome;
-    }
+    @Schema(description = "Endereço de e-mail do cliente", example = "cliente@email.com")
+    private String email;
 
-    public Long getId() {
-        return id;
-    }
+    @Schema(description = "Número de telefone do cliente", format = "tel")
+    private String telefone;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+    
+    
 }
